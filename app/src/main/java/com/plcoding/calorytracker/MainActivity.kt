2755fafec4a2977bea637b040ea433b435a268fd
João Graça <joaograca360@gmail.com>
2022-05-26 16:22:59 +0100

    Add WelcomeScreen

--- app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
+++ app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
@@ -3,11 +3,7 @@ package com.plcoding.calorytracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import com.joaograca.onboarding_presentation.welcome.WelcomeScreen
 import com.plcoding.calorytracker.ui.theme.CaloryTrackerTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,7 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CaloryTrackerTheme {
-
+                WelcomeScreen()
             }
         }
     }

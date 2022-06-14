ec07bd552581292cba97bd0bc941667fb63e5678
João Graça <joaograca360@gmail.com>
2022-06-14 11:47:05 +0100

    Add DaySelector to TrackerOverviewScreen

--- tracker/tracker_presentation/src/main/java/com/joaograca/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/joaograca/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -1,7 +1,6 @@
 package com.joaograca.tracker_presentation.tracker_overview
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -9,6 +8,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.joaograca.core.util.UiEvent
 import com.joaograca.core_ui.LocalSpacing
+import com.joaograca.tracker_presentation.tracker_overview.components.DaySelector
 import com.joaograca.tracker_presentation.tracker_overview.components.NutrientsHeader
 
 @Composable
@@ -24,6 +24,22 @@ fun TrackerOverviewScreen(
             .fillMaxSize()
             .padding(bottom = spacing.spaceMedium)
     ) {
-        item { NutrientsHeader(state = state) }
+        item {
+            NutrientsHeader(state = state)
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+            DaySelector(
+                date = state.date,
+                onPreviousDayClick = {
+                    viewModel.onEvent(TrackerOverviewEvent.OnPreviousDayClick)
+                },
+                onNextDayClick = {
+                    viewModel.onEvent(TrackerOverviewEvent.OnNextDayClick)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = spacing.spaceMedium)
+            )
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+        }
     }
 }
